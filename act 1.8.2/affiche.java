f9f310d395f55e9547b0e2e705d5bc590c91d805
Azizelouni <103032007+Azizelouni@users.noreply.github.com>
2022-04-14 15:34:08 +0100

    Update affiche.java

--- act 1.8.2/affiche.java
+++ act 1.8.2/affiche.java
@@ -1,49 +1,92 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
-public class PlayListeUI {
+import java.io.*;
+import java.util.*;
 
 
+
+public class Main {
 public static void main(String[] args) {
 
 
-MusiqueImpl music1 = new MusiqueImpl("Billie Jean", "MJ", "interprete1", "POP");
-MusiqueImpl music2 = new MusiqueImpl("Godzilla","Eminem", "interprete2", "RAP");
-MusiqueImpl music3 = new MusiqueImpl("Keys to Imagination", "YANNI", "interprete3", "INSTRUMENTAL");
+
+ArrayList<MusiqueImpl> m = new ArrayList<MusiqueImpl>();
+int i=0;
 
 
-MusiqueImpl music4 = new MusiqueImpl("maad city", "kendrick lamar", "interprete4", "RAP");
-MusiqueImpl music5 = new MusiqueImpl("freedom","pharell", "interprete5", "RNB");
-MusiqueImpl music6 = new MusiqueImpl("all about us", "Tatu", "interprete6", "RNB");
 
 
-List<MusiqueImpl> myList1 = new ArrayList<MusiqueImpl>() ;
-List<MusiqueImpl> myList2 = new ArrayList<MusiqueImpl>() ;
+// Création d’un fileReader pour lire le fichier
+FileReader fileReader = new FileReader(“/path/to/the/file”);
 
-PlayListe PlayListe1=new PlayListeImpl("My Hits", "Hits",5,myList1);
-PlayListe PlayListe2=new PlayListeImpl("My Hits", "Hits",5,myList2);
 
-Scanner sc = new Scanner(System.in);
 
-System.out.println("Affichage des playlists disponibles : \n");
+// Création d’un bufferedReader qui utilise le fileReader
+BufferedReader reader = new BufferedReader (fileReader);
 
-PlayListe1.AjouterMusiquePlaylist(music1);
-PlayListe1.AjouterMusiquePlaylist(music2);
-PlayListe1.AjouterMusiquePlaylist(music3);
 
-PlayListe2.AjouterMusiquePlaylist(music4);
-PlayListe2.AjouterMusiquePlaylist(music5);
-PlayListe2.AjouterMusiquePlaylist(music6);
 
-myList1.addAll(myList2);
-PlayListe1.afficherPlayListe();
 
-System.out.println("\ndonner le titre ou l'auteur de la musique : ");
-String musique = sc.nextLine();
+try {
+
+
+
+// une fonction à essayer pouvant générer une erreur
+line = reader.readLine();
+
+
+
+while(line != null) {
+
+
+
+// lit les musiques a insérer depuis un fichier
+m.titre = entree.next();
+m.autheur = entree.next();
+m.interprete = entree.next();
+m.genre = entree.next();
+arraylist.add(m);
+i++;
+
+
+
+// lecture de la prochaine ligne
+line = reader.readLine();
+}
+
+
+
+
+catch (IOException e) {
+
+
+
+e.printStackTrace();
+
+
 
 
 }
+
+
+
+reader.close();
+
+
+
+set.CollAhmed = CollAhmed.(m);
+List CollAhmed = Collections.synchronizedList(new ArrayList());
+
+
+
+//enregistre la playlist d'un utilisateur dans un autre
+int index=System.out.print("donner l'index du playlist choisit pour l'enregister");
+set.CollAhmed = CollAli.(m);
+List CollAli = Collections.synchronizedList(new ArrayList());
+
+
+
+}
+
+
 
 }
 
