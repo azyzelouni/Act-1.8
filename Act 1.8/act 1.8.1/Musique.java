86a3be693315f5e7ba230c4b661b8fef06534c7e
Azizelouni <103032007+Azizelouni@users.noreply.github.com>
2022-04-14 15:31:04 +0100

    Delete Musique.java

--- Act 1.8/act 1.8.1/Musique.java
+++
@@ -1,8 +0,0 @@
-package ProjetMusique;
-
-public interface Musique {
-	
-	public String afficherMusique(MusiqueImpl musique);
-	
-	public boolean comparerMusique(MusiqueImpl musique1, MusiqueImpl musique2);
-}
